6c3e6c8e40e47e1883081a51b99656cdc1c504ab
Eghosa Ordia <eghordia130@gmail.com>
2025-01-23 21:29:42 +0100

    created a schedule tweet model

--- x_manager/models.py
+++ x_manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
+
+class ScheduledTweet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tweet_text = models.CharField(max_length=280)
+    schedule_time = models.DateTimeField()
+    is_posted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.tweet_text} - {self.user.username}"
